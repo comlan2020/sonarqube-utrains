4d0a205cdf0c9236ccb3bfaaa37ef7a0133ca69d
comlan2020 <115726965+comlan2020@users.noreply.github.com>
2022-10-25 23:47:19 -0400

    Update sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,5 +1,3 @@
-#!bin/bash
-
 ## Sonarqube install on centos 7
 ## Please run this as user vagrant
 
@@ -15,27 +13,17 @@ exit 1
 fi
 
 sudo yum update -y
-
 sudo yum install java-11-openjdk-devel -y
-
 sudo yum install java-11-openjdk -y
-
 cd /opt
-
 sudo yum install wget unzip -y
-
 sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.3.0.51899.zip
-
 sudo unzip /opt/sonarqube-9.3.0.51899.zip
-
 sudo chown -R vagrant:vagrant /opt/sonarqube-9.3.0.51899
-
-sudo chown -R vagrant:vagrant /opt/sonarqube-9.3.0.51899
-
- ./sonar.sh start
-
+cd /opt/sonarqube-9.3.0.51899/bin/linux-x86-64/
 sudo firewall-cmd --permanent --add-port=9000/tcp
 sudo firewall-cmd --reload
  ./sonar.sh start
 echo "Installation done successfully please get the ip and access your sonarqube on the browser"
-  
+
+
